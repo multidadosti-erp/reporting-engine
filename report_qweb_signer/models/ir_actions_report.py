e4e6471e2f464c2f41a52019ef8a3fcf98330372
Michell Stuttgart <michellstut@gmail.com>
2024-05-24 11:50:59 -0300

    Migração para Python 3.10

--- report_qweb_signer/models/ir_actions_report.py
+++ report_qweb_signer/models/ir_actions_report.py
@@ -82,7 +82,7 @@ class IrActionsReport(models.Model):
             ('res_id', '=', res_ids[0]),
         ], limit=1)
         if attachment:
-            return base64.decodestring(attachment.datas)
+            return base64.decodebytes(attachment.datas)
         return False
 
     def _attach_signed_write(self, res_ids, certificate, signed):
